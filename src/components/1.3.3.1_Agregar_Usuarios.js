f22c5f9ecfa1fb54092fd992dd9569cd48b5ad80
j1arm1 <90942808+j1arm1@users.noreply.github.com>
2021-12-07 16:10:58 -0500

    Update 1.3.3.1_Agregar_Usuarios.js

--- src/components/1.3.3.1_Agregar_Usuarios.js
+++ src/components/1.3.3.1_Agregar_Usuarios.js
@@ -5,16 +5,52 @@ const AgregarUsuarios = function (props) {
     return (
         
             <div className="card">
-                <div class="card-header bg-primary text-white">
-                    <h1>Agregar Usuarios</h1>
-                </div>
-                <div className="card-body">
-                    
-                    <p>
-                    Jesus 
-                    </p>
-                </div>
-            </div>
+          <div>
+            <h3>Crear Nuevo Usuario</h3>
+          </div>
+          <div className="form-group">
+            <label>ID</label>
+            <input
+              className="form-control"
+              readOnly
+              type="text"
+              name="id"
+              value=""
+            />
+            <br />
+
+            <label>Usuario</label>
+            <input
+              className="form-control"
+              type="text"
+              name="usuario"
+              value=""
+            />
+            <br />
+
+            <label>Nombres</label>
+            <input
+              className="form-control"
+              type="text"
+              name="nombres"
+              value=""
+            />
+            <br />
+
+            <label>Tipo de Usuario</label>
+            <input
+              className="form-control"
+              type="text"
+              name="rol"
+              value=""
+            />
+            <br />
+
+          </div>
+          <button className="btn btn-primary">Agregar Usuario</button>
+          <button className="btn btn-danger">Cancelar</button>
+
+        </div>
        
     );
 
